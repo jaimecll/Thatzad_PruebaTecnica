d84bf3657cd789405e27a5622411a2504c8fbea9
jaimecll <jaimecgfg@gmail.com>
2020-02-15 23:58:41 +0100

    Estilos finalizados y validacion

---
+++ home.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/845eda249f.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="./js/javascript.js"></script>
+
+    <title>Thatzad Weather</title>
+  </head>
+  <body>
+
+<div class="d-flex justify-content-center">
+      <div class="p-2" ><img class="Logo" src="./img/Bitmap.png"><p class="Texto3">¡Que la lluvia no te pare!</p></div>
+
+  </div>
+  <div style="margin-top: -50px;">
+      <div class="modal-body row" style="margin:50px;">
+  <div class="columna1">
+    <p>hola1</p>
+  </div>
+  <div class="columna2">
+    <p>hola2</p>
+  </div>
+</div>
+</div>
+</div>
+    
+
+   
+  </body>
+</html>
--- index.php
+++ index.php
@@ -9,18 +9,34 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet"> 
     <script src="https://kit.fontawesome.com/845eda249f.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="./js/javascript.js"></script>
 
     <title>Thatzad Weather</title>
   </head>
   <body>
-
+  	<!--Columna central con bootstrap-->
   	<div class="d-flex justify-content-center">
   		<div class="d-flex flex-column">
  			<div class="p-2"><img class="Logo" src="./img/Bitmap.png"></div>
+
+
+
+ 			<div class="p-2"><p id="alerta" style="color:red; text-align: center; font-family: Muli; font-size: 24px;"></p></div>
   			<div class="p-2"><p class="Texto1">Entérate del tiempo en la zona exacta que te interesa buscando por código postal.</p></div>
+
+
+
+  			<!--Formulario para introducir codigo postal-->
+  			<form id='inicio' action='home.php' method='POST' accept-charset='UTF-8' onsubmit = "return ValidacionCP()">
   			<div class="p-2"><input type="text" class="CodigoPostal" name="CodigoPostal" id="CodigoPostal" placeholder="Introduce el código postal"></div>
   			<!--<div class="p-2"><div class="Buscar" onclick=""><p class="TextoBoton">Buscar<span class="Lupa"><i class="fas fa-search-location fa-4x"></i></span></p></div></div>-->
   			<div class="p-2"><button class="Buscar" type="submit" name="eviar"><p class="TextoBoton">Buscar<span class="Lupa"><i class="fas fa-search-location fa-4x"></i></span></p></button></div>
+  			</form>
+
+
+
+
+
   			<div class="p-2"><p class="Texto2">¡Que la lluvia no te pare!</p></div>
 		</div>
   		
@@ -30,8 +46,6 @@
   	</div>
     
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   
   </body>
 </html>
