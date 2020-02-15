20ea6edb236fd36716bf2734b0090f124b6ce300
jaimecll <jaimecgfg@gmail.com>
2020-02-15 10:56:15 +0100

    Subida inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thatzad OpenWeatherMap</title>
+</head>
+<body>
+	<form action="codPostal.proc.php" method="POST">
+		<input type="number" id="codPostal" name="codPostal" required="">
+		<input type="submit">
+	</form>
+</body>
+</html>
